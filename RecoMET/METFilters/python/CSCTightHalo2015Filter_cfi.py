9b00ed81d14e6793c4bb796d19f7cc6f23691095
Cms Build <cmsbuild@cern.ch>
2015-09-30 10:43:19 +0200

    Resolve the conflicts; Deleted non framework related updates.

--- RecoMET/METFilters/python/CSCTightHalo2015Filter_cfi.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-CSCTightHalo2015Filter = cms.EDFilter(
-  "CSCTightHalo2015Filter",
-  taggingMode = cms.bool(False)
-)
--- RecoMET/METFilters/python/CSCTightHaloTrkMuUnvetoFilter_cfi.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-CSCTightHaloTrkMuUnvetoFilter = cms.EDFilter(
-  "CSCTightHaloTrkMuUnvetoFilter",
-  taggingMode = cms.bool(False),
-)
