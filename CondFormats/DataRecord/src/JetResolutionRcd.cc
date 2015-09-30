ba9c1056c25b9170efe9145815d9044f1a748320
Cms Build <cmsbuild@cern.ch>
2015-09-30 14:42:49 +0200

    Resolve the conflicts; Deleted non framework related updates.

--- CondFormats/DataRecord/src/JetResolutionRcd.cc
+++
@@ -1,7 +0,0 @@
-// -*- C++ -*-
-// Author: Sébastien Brochet
-
-#include "CondFormats/DataRecord/interface/JetResolutionRcd.h"
-#include "FWCore/Framework/interface/eventsetuprecord_registration_macro.h"
-
-EVENTSETUP_RECORD_REG(JetResolutionRcd);
--- JetMETCorrections/Modules/python/JetResolutionESProducer_cfi.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-JetResolutionESProducer_AK4PFchs = cms.ESProducer("JetResolutionESProducer",
-        label = cms.string('AK4PFchs')
-)
-
-JetResolutionESProducer_SF_AK4PFchs = cms.ESProducer("JetResolutionScaleFactorESProducer",
-        label = cms.string('AK4PFchs')
-)
