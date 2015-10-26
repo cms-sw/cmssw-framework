b2b6d295e0be69010e0ed0d4bc05d13eaa256f81
Cms Build <cmsbuild@cern.ch>
2015-10-26 16:45:33 +0100

    Resolve the conflicts; Deleted non framework related updates.

--- PhysicsTools/TagAndProbe/src/classes.h
+++
@@ -1,9 +0,0 @@
-#include "PhysicsTools/TagAndProbe/interface/RooCBExGaussShape.h"
-#include "PhysicsTools/TagAndProbe/interface/ZGeneratorLineShape.h"
-#include "PhysicsTools/TagAndProbe/interface/RooCMSShape.h"
-#include "TVirtualFFT.h"
-
-namespace PhysicsTools_TagAndProbe {
-  struct dictionary {
-  };
-}
