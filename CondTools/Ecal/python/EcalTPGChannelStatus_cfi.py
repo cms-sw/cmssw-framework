975004be8c81253bc79815e1092ed8d34b342818
Cms Build <cmsbuild@cern.ch>
2015-11-26 05:42:39 +0100

    Resolve the conflicts; Deleted non framework related updates.

--- CondTools/Ecal/python/EcalTPGChannelStatus_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-EcalTrivialConditionRetriever = cms.ESSource("EcalTrivialConditionRetriever",
-  producedEcalTrgChannelStatus = cms.untracked.bool(True),
-#  trgChannelStatusFile = cms.untracked.string('CalibCalorimetry/EcalTrivialCondModules/data/trgChannelStatus.txt')
-#  trgChannelStatusFile = cms.untracked.string('CalibCalorimetry/EcalTrivialCondModules/data/triggermasks_confid722.txt')
-  trgChannelStatusFile = cms.untracked.string('CalibCalorimetry/EcalTrivialCondModules/data/triggermasks_confid722_plus_ebm10_tt64.txt')
-)
--- CondTools/Ecal/python/EcalTPGTowerStatus_cfi.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-EcalTrivialConditionRetriever = cms.ESSource("EcalTrivialConditionRetriever",
-  producedEcalTrgTowerStatus = cms.untracked.bool(True),
-  trgTowerStatusFile = cms.untracked.string('CondTools/Ecal/python/BTT_DATA_CONF_ID_2982.dat')
-)
