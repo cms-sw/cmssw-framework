3ab80af92e1772d735c878ff3aa94218c366cab6
Cms Build <cmsbuild@cern.ch>
2015-12-03 16:44:03 +0100

    Resolve the conflicts; Deleted non framework related updates.

--- Validation/RecoEgamma/python/photonMiniAODValidationSequence_cff.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from Validation.RecoEgamma.photonValidatorMiniAOD_cfi import *
-
-
-photonMiniAODValidationSequence = cms.Sequence(photonValidationMiniAOD)
-
-
