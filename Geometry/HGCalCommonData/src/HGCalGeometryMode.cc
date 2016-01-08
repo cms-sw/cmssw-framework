51abb2a6fa10eb1e578fcd869ee32d76618e9941
Cms Build <cmsbuild@cern.ch>
2016-01-08 17:42:55 +0100

    Resolve the conflicts; Deleted non framework related updates.

--- Geometry/HGCalCommonData/src/HGCalGeometryMode.cc
+++
@@ -1,8 +0,0 @@
-#include "Geometry/HGCalCommonData/interface/HGCalGeometryMode.h"
-
-template<>
-HGCalStringToEnumParser<HGCalGeometryMode::GeometryMode>::HGCalStringToEnumParser() {
-  enumMap["HGCalGeometryMode::Square"]  = HGCalGeometryMode::Square;
-  enumMap["HGCalGeometryMode::Hexagon"] = HGCalGeometryMode::Hexagon;
-}
-
