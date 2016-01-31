47f5e17b759e0c087227dfbaad1233a0bdc71ce9
Cms Build <cmsbuild@cern.ch>
2016-01-31 21:43:05 +0100

    Resolve the conflicts; Deleted non framework related updates.

--- RecoLocalTracker/Phase2TrackerRecHits/python/Phase2StripCPEESProducer_cfi.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-phase2StripCPEESProducer = cms.ESProducer("Phase2StripCPEESProducer",
-  ComponentType = cms.string('Phase2StripCPEDummy'),
-  parameters    = cms.PSet()
-)
