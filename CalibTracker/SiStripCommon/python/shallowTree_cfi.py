47371a14d6ff895e4be27a9808d40bd562f1dc4d
Cms Build <cmsbuild@cern.ch>
2016-02-03 14:40:41 +0100

    Resolve the conflicts; Deleted non framework related updates.

--- CalibTracker/SiStripCommon/python/shallowTree_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-shallowTree = cms.EDAnalyzer(
-   "ShallowTree",
-   outputCommands = cms.untracked.vstring(
-      'drop *',
-      )
-   )
--- CalibTracker/SiStripCommon/test/test_all.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-
-echo "TESTING CalibTracker/SiStripCommon ..."
-cmsRun ${LOCAL_TEST_DIR}/test_all_cfg.py
-echo "... SUCCESSFUL!"
