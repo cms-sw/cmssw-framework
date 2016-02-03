b868dd6050f9592cc07b5fef081d3be33e51cc72
Cms Build <cmsbuild@cern.ch>
2016-02-03 18:40:49 +0100

    Resolve the conflicts; Deleted non framework related updates.

--- L1Trigger/L1TNtuples/README.md
+++
@@ -1,6 +0,0 @@
-L1Ntuples
-=========
-
-Tool to build L1 DPG ntuples used in LHC run 1  
-
-Ducumentation Twiki Page: https://twiki.cern.ch/twiki/bin/viewauth/CMS/L1TriggerDPGNtupleProduction
--- L1Trigger/L1TNtuples/python/l1MenuTree_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-l1MenuTree = cms.EDAnalyzer("L1MenuTreeProducer",
-   l1GtRecordInputTag = cms.InputTag("l1GtTriggerMenuLite"),
-   l1GtReadoutRecordInputTag = cms.InputTag(""),
-   l1GtTriggerMenuLiteInputTag =  cms.InputTag("l1GtTriggerMenuLite"),
-)
-
