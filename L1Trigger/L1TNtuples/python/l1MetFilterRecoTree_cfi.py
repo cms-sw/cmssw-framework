c214fe9d1a5e46517258c710ff379f316d66c75c
Cms Build <cmsbuild@cern.ch>
2016-02-06 10:40:46 +0100

    Resolve the conflicts; Deleted non framework related updates.

--- L1Trigger/L1TNtuples/python/l1MetFilterRecoTree_cfi.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-
-l1MetFilterRecoTree = cms.EDAnalyzer("L1MetFilterRecoTreeProducer",
-  triggerResultsToken               = cms.untracked.InputTag("TriggerResults"),
-  hbheNoiseFilterResultToken        = cms.untracked.InputTag("HBHENoiseFilterResult")
- )
-
-
