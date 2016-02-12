b3f5b22fd26330ca473a70a0420905ad85e069f3
Cms Build <cmsbuild@cern.ch>
2016-02-12 20:41:17 +0100

    Resolve the conflicts; Deleted non framework related updates.

--- RecoBTag/SecondaryVertex/python/pfSimpleInclusiveSecondaryVertexHighEffBJetTags_cfi.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-pfSimpleInclusiveSecondaryVertexHighEffBJetTags = cms.EDProducer("JetTagProducer",
-	jetTagComputer = cms.string('candidateSimpleSecondaryVertex2TrkComputer'),
-	tagInfos = cms.VInputTag(cms.InputTag("pfInclusiveSecondaryVertexFinderTagInfos"))
-)
