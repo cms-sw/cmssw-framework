e2b858f3bda7f7441217daceb7e90c4a9b5251d4
Cms Build <cmsbuild@cern.ch>
2016-04-12 16:51:26 +0200

    Resolve the conflicts; Deleted non framework related updates.

--- L1Trigger/L1THGCal/python/hgcalTriggerPrimitives_cff.py
+++
@@ -1,5 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from L1Trigger.L1THGCal.hgcalTriggerPrimitiveDigiProducer_cfi import *
-
-hgcalTriggerPrimitives = cms.Sequence(hgcalTriggerPrimitiveDigiProducer)
