dd13a47e6a0eb9c9fead56e059609217a32514b7
Cms Build <cmsbuild@cern.ch>
2016-04-13 10:41:57 +0200

    Resolve the conflicts; Deleted non framework related updates.

--- Alignment/MillePedeAlignmentAlgorithm/python/MillePedeDQMModule_cff.py
+++
@@ -1,7 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-import  Alignment.MillePedeAlignmentAlgorithm.MillePedeFileReader_cfi as MillePedeFileReader_cfi
-
-SiPixelAliDQMModule = cms.EDAnalyzer("MillePedeDQMModule",
-                                     MillePedeFileReader = cms.PSet(MillePedeFileReader_cfi.MillePedeFileReader.clone())
-    )
--- L1Trigger/L1TGlobal/python/l1GtExtCondLegacyToStage2_cfi.py
+++
@@ -1,9 +0,0 @@
-
-import FWCore.ParameterSet.Config as cms
-
-l1GtExtCondLegacyToStage2 = cms.EDProducer("l1t::GtExtCondLegacyToStage2",
-                                           bxFirst = cms.int32(-2),
-                                           bxLast = cms.int32(2),
-                                           LegacyGtReadoutRecord = cms.InputTag("unpackLegacyGtDigis")
-)
-
