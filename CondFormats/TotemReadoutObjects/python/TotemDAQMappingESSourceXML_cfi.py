a1dd117166ae84efd3e7015a39dbe1464be844de
Cms Build <cmsbuild@cern.ch>
2016-04-19 11:47:54 +0200

    Resolve the conflicts; Deleted non framework related updates.

--- CondFormats/TotemReadoutObjects/python/TotemDAQMappingESSourceXML_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-TotemDAQMappingESSourceXML = cms.ESSource("TotemDAQMappingESSourceXML",
-  verbosity = cms.untracked.uint32(0),
-
-  mappingFileNames = cms.untracked.vstring(),
-  maskFileNames = cms.untracked.vstring()
-)
