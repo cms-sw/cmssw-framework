effcc848ccd7674896bacd5df4f4bce0619deac6
Cms Build <cmsbuild@cern.ch>
2016-04-20 08:40:50 +0200

    Resolve the conflicts; Deleted non framework related updates.

--- RecoMET/METFilters/python/globalSuperTightHalo2016Filter_cfi.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-globalSuperTightHalo2016Filter = cms.EDFilter(
-  "GlobalSuperTightHalo2016Filter",
-  taggingMode = cms.bool(False)
-)
--- RecoMET/METFilters/python/globalTightHalo2016Filter_cfi.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-globalTightHalo2016Filter = cms.EDFilter(
-  "GlobalTightHalo2016Filter",
-  taggingMode = cms.bool(False)
-)
