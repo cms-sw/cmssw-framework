f356d154f0e02b87247163a9165af7c789553bfe
Cms Build <cmsbuild@cern.ch>
2016-04-22 21:01:24 +0200

    Resolve the conflicts; Deleted non framework related updates.

--- Validation/HGCalValidation/python/hgcGeometryClient_cfi.py
+++
@@ -1,5 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-hgcalGeometryClient = cms.EDAnalyzer("HGCalGeometryClient", 
-                                     DirectoryName = cms.string("Geometry"),
-                                     )
